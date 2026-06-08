69905c9236ffb0e81c05aed185237e78882e9c02
Hemnath P <hemnathyogeshwaran@gmail.com>
2026-06-08 16:16:29 +0530

    Add MongoDB connection setup in db.js

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://127.0.0.1:27017/taskmanager"
+    );
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDB;
